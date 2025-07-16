df74550f6e71b424d656cc7dfd19fa148d98c850
Thiago <thiagopersiadm@gmail.com>
2025-07-16 15:43:05 -0300

    Resolvendo Bugs

--- src/main/java/br/com/refspring/test/SpringSemWebApplication.java
+++ src/main/java/br/com/refspring/test/SpringSemWebApplication.java
@@ -6,9 +6,11 @@ import br.com.refspring.test.model.DadosTemporada;
 import br.com.refspring.test.service.APIConsumer;
 import br.com.refspring.test.service.ConvertData;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -31,35 +33,30 @@ public class SpringSemWebApplication implements CommandLineRunner {
         ConvertData conversor = new ConvertData();
         DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
         System.out.println(dados);
-        json = consumeAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&Episode=2&apikey=96feb9b7"); //add url com episodio
+
+        json = consumer.consumeAPI("https://www.omdbapi.com/?t=gilmore+girls&Season=1&Episode=2&apikey=96feb9b7"); //add url com episodio
         DadosEpsodios dadosEps = conversor.obterDados(json, DadosEpsodios.class);
         System.out.print(dadosEps);
 
-        temporadas.forEach(System.out::println);
 
-
-
-        temporadas.ForEach(f -> f.episodio().ForEach(e -> System.out.println(e.titulo())))
-        //código Stream acima equivale ao código abaixo
-        
-       /* List<DadosTemporada> temporadas = new ArrayList<>();
-        for(int i = 1; i<dados.totalTemporadas(); i++){
-            json = consumeAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=96feb9b7"); //dentro da URL trocar o numero de temporadas por i
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        for(int i = 1; i<dados.totalTemporadas(); i++) {
+            json = consumer.consumeAPI("https://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=96feb9b7"); //dentro da URL trocar o numero de temporadas por i
             DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
             temporadas.add(dadosTemporada);
-        }*/
+        }
+        temporadas.forEach(System.out::println);
+
+        temporadas.forEach(f -> f.episodio().forEach(e -> System.out.println(e.titulo())));
 
        //pegar os 5 melhores epsódios de uma série
        List<DadosEpsodios> epsodiosDados = temporadas.stream()
-                                            .flatMap(t-> t.epsódios().stream())
+                                            .flatMap(t-> t.episodio().stream())
                                             .collect(Collectors.toList());
 
-        epsodiosDados.stream()
-        .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
-        .sorted(Comparator.comparing(DadosEpsodios::avaliacao)
-        .reversed())
-        .limited(5)
-        .ForEach(System.out::println);
+        epsodiosDados.stream().filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpsodios::avaliacao)
+                        .reversed()).limit(5).forEach(System.out::println);
                                             
 
         
--- src/main/java/br/com/refspring/test/model/DadosEpsodios.java
+++ src/main/java/br/com/refspring/test/model/DadosEpsodios.java
@@ -1,3 +1,8 @@
+package br.com.refspring.test.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosEpsodios(@JsonAlias("Title") String titulo,
                             @JsonAlias("Episode") int numeroEps, 
